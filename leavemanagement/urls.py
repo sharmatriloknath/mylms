f0c02600daf365aac278ddb8a8db1639dd1217f7
sharmatriloknath <sharmatriloknath336@gmail.com>
2020-01-29 00:04:42 +0530

    this is my first commit

---
+++ leavemanagement/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token,verify_jwt_token
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth-jwt/', obtain_jwt_token),
+    path('auth-jwt-refresh/', refresh_jwt_token),
+    path('auth-jwt-verify/', verify_jwt_token),
+]
